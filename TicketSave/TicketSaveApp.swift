6febb7ef11198354a13688fc19e9912e9a39e857
JettyCoffee <jetty_coffee@outlook.com>
2026-03-09 01:20:40 +0800

    Initial Commit

---
+++ TicketSave/TicketSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketSaveApp.swift
+//  TicketSave
+//
+//  Created by JettyCoffee on 2026/3/9.
+//
+
+import SwiftUI
+
+@main
+struct TicketSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
